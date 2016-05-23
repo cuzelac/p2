866c9a15553c20861b3df22c57b88cdb7534e572
Chris Uzelac <cu@squareup.com>
2016-05-23 15:59:28 -0700

    implement all in go

--- bin/glue/hupper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#ls -ltr
-nohup sleep 100 & # HANG
-#nohup sleep 100 </dev/null >/dev/null 2>/dev/null & # NO HANG
--- bin/glue/main.go
+++ bin/glue/main.go
@@ -1,23 +1,34 @@
 package main
 
 import (
-	//"github.com/square/p2/pkg/hooks"
-	//"io/ioutil"
-	"bytes"
-	"io"
+	"github.com/square/p2/pkg/hooks"
+	"github.com/square/p2/pkg/logging"
+
+	"io/ioutil"
+	"path/filepath"
+
 	"os"
-	"os/exec"
 )
 
 func main() {
-	//hooks.RunHook('
-	out := &bytes.Buffer{}
+	tmpfile := writeBash("nohup sleep 100 &") // HANG
+	//tmpfile := writeBash("nohup sleep 100 >/dev/null 2>/dev/null &") // NO HANG
+	defer os.Remove(tmpfile.Name())
 
-	cmd := exec.Command("./hupper.sh")
-	//cmd.Stdin = nil
-	cmd.Stdout = out
-	cmd.Stderr = out
-	cmd.Run()
+	fullPath, _ := filepath.Abs(tmpfile.Name())
 
-	io.Copy(os.Stdout, out)
+	hooks.RunHook(fullPath, "nohup-hook", []string{"a", "b"}, logging.TestLogger())
+}
+
+func writeBash(line string) *os.File {
+	tmpfile, _ := ioutil.TempFile(".", "runner.")
+
+	tmpfile.Write([]byte("#!/bin/bash\n"))
+	tmpfile.Write([]byte(line))
+	tmpfile.Write([]byte("\n"))
+	tmpfile.Close()
+
+	os.Chmod(tmpfile.Name(), 0744)
+
+	return tmpfile
 }
